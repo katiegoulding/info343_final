3a4fc33066efb2397edf60977c9be6a2db0769f9
August Carow <august.carow@gmail.com>
2017-12-01 09:11:45 -0800

    add starter files for react and firebase

---
+++ timer/src/components/SignInForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import firebase from "firebase/app";
+
+export default class SignIn extends React.Component {
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}    
---
+++ timer/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import firebase from "firebase/app"
+import "firebase/auth";
+import "firebase/database";
+
+var config = {
+    apiKey: "AIzaSyARE45Qi4idoyl8i_k2_Z49bOamBCrwq00",
+    authDomain: "final-project-343.firebaseapp.com",
+    databaseURL: "https://final-project-343.firebaseio.com",
+    projectId: "final-project-343",
+    messagingSenderId: "74000220836"
+};
+
+firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
