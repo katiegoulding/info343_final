f353203c88ef6e046bf22619964e48846d8ec0ac
Amanda Shen <cnshen28@uw.edu>
2017-12-04 23:14:57 -0800

    Amanda2

--- timer/src/components/HeaderBar2.jsx
+++ timer/src/components/HeaderBar2.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from '../images/RealLogo.svg';
-import logo2 from '../images/splish.png';
+import logo from '../images/splish+logo.png';
+
 import { Link } from "react-router-dom";
 import constants from './constants';
 import "./style.css";
@@ -12,8 +12,7 @@ export default class HeaderBar extends React.Component {
                 <div id="headerBarLogo">
                     <div id="LOGO">
                         <Link to={constants.routes.home}>
-                            <img src={logo} width="53" height="70" className="d-inline-block align-top" alt=""/>
-                            <img src={logo2} className="d-inline-block align-top pl-3" alt=""/>
+                            <img src={logo} width="231px" className="d-inline-block align-top" alt=""/>
                         </Link>
                     </div>
                 </div>                       
